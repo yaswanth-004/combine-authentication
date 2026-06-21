9486c1c593b9dbd0c25911a160b341cb053c77fd
Mallidi Yaswanth <145952092+yaswanth-004@users.noreply.github.com>
2026-06-21 17:43:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,8 +23,9 @@ class Config:
     AUTH0_DOMAIN        = os.environ.get("AUTH0_DOMAIN", "dev-16cf0iiguw088wbm.au.auth0.com")
     AUTH0_CLIENT_ID     = os.environ.get("AUTH0_CLIENT_ID")
     AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
-    AUTH0_CALLBACK_URL  = os.environ.get("AUTH0_CALLBACK_URL",  "https://combine-authentication.vercel.app/callback")
-    APP_BASE_URL        = os.environ.get("APP_BASE_URL",        "https://combine-authentication.vercel.app")
+    AUTH0_CALLBACK_URL  = os.environ.get("AUTH0_CALLBACK_URL", "https://combine-authentication-a4vl.vercel.app/callback")
+    APP_BASE_URL        = os.environ.get("APP_BASE_URL",  "https://combine-authentication-a4vl.vercel.app")
+  
 
     # Token settings (mirrors what you set in Auth0 dashboard)
     ACCESS_TOKEN_LIFETIME_SECONDS  = 86400      # 1 day
